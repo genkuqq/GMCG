9311fe698fdb7e275266724340f23744fb37b6f7
genkuqq <koraygench@gmail.com>
2024-11-05 14:25:41 +0300

    add: client | simple packet sending

--- client/multitest.gd
+++ client/multitest.gd
@@ -2,6 +2,7 @@ extends Node
 
 var PORT = 1234
 var ADDRESS = "127.0.0.1"
+var peer
 
 func _ready() -> void:
 	var peer = ENetMultiplayerPeer.new()
@@ -12,3 +13,8 @@ func _ready() -> void:
 	multiplayer.multiplayer_peer = peer
 	peer.peer_connected.connect(func(id): print("Succesfully connected."))
 	peer.peer_disconnected.connect(func(id): print("Succesfully disconnected."))
+
+func _process(delta) -> void:
+	if multiplayer.multiplayer_peer.CONNECTION_CONNECTED:
+		var packet = "hello"
+		multiplayer.send_bytes(packet.to_utf8_buffer(),1);
