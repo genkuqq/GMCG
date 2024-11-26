38053846f82b8c8b34333db75782c9a6fa659e74
genkuqq <koraygench@gmail.com>
2024-11-26 21:23:02 +0300

    fix: few code issues

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,10 +1,10 @@
-extern crate enet;
-
 use enet::*;
 use std::net::Ipv4Addr;
 use std::time::Duration;
 
-fn main() -> Result<(), Error> {
+mod types;
+
+fn main(){
     let enet = Enet::new().expect("could not initialize ENet");
 
     let local_addr = Address::new(Ipv4Addr::LOCALHOST, 9001);
@@ -20,34 +20,27 @@ fn main() -> Result<(), Error> {
         .expect("could not create host");
 
     loop {
-        // Wait 1 second for any events.
-        match host.service(Duration::from_secs(1)) {
-            Ok(Some(event)) => match &event.kind {
-                &EventKind::Connect => {
+        if let Ok(Some(event)) = host.service(Duration::from_secs(1)) {
+            match &event.kind {
+                EventKind::Connect => {
                     println!("new connection!");
                     let id = event.peer_id;
-                    print!("{:#?}",id);
-                },
-                &EventKind::Disconnect { .. } => println!("disconnect!"),
-                &EventKind::Receive {
-                    ref channel_id,
-                    ref packet,
+                    print!("{:#?}", id);
+                }
+                EventKind::Disconnect { .. } => println!("disconnect!"),
+                EventKind::Receive {
+                    channel_id,
+                    packet,
                 } => {
                     println!(
-                    "got packet on channel {}, content: '{}'",
-                    channel_id,
-                    std::str::from_utf8(packet.data()).unwrap());
+                        "got packet on channel {}, content: '{}'",
+                        channel_id,
+                        std::str::from_utf8(packet.data()).unwrap()
+                    );
                     let a = event.peer_id;
-                    print!("{:#?}", a)
-                },
-            },
-            Ok(None) => {
-                // No event occurred within the timeout duration
-            }
-            Err(e) => {
-                // Handle the error from `service`
-                eprintln!("Error servicing host: {:?}", e);
+                    print!("{:#?}", a);
+                }
             }
         }
-    }
+    }   
 }
