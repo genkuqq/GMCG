0dec6c9606ae84144a072d07c765fbc3ba1cc1cf
genkuqq <koraygench@gmail.com>
2024-11-26 10:37:38 +0300

    upd: using updated enet version again

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,53 +1,53 @@
-use std::net::UdpSocket;
-use std::time::{Duration,SystemTime,Instant};
-use renet::{ConnectionConfig,DefaultChannel,RenetServer,ServerEvent};
-use renet::transport::{NetcodeServerTransport,ServerAuthentication,ServerConfig};
+extern crate enet;
 
-mod types;
+use enet::*;
+use std::net::Ipv4Addr;
+use std::time::Duration;
 
+fn main() -> Result<(), Error> {
+    let enet = Enet::new().expect("could not initialize ENet");
 
-fn main(){
-    let mut server = RenetServer::new(ConnectionConfig::default());
-    let socket: UdpSocket = UdpSocket::bind("127.0.0.1:1234").unwrap();
-    let server_config = ServerConfig {
-        current_time: SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap(),
-        max_clients: 64,
-        protocol_id: 0,
-        public_addresses: vec![socket.local_addr().unwrap()],
-        authentication: ServerAuthentication::Unsecure
-    };
-    let mut transport = NetcodeServerTransport::new(server_config, socket).unwrap();
-    let mut last_updated = Instant::now();
+    let local_addr = Address::new(Ipv4Addr::LOCALHOST, 9001);
+
+    let mut host = enet
+        .create_host::<()>(
+            Some(&local_addr),
+            10,
+            ChannelLimit::Maximum,
+            BandwidthLimit::Unlimited,
+            BandwidthLimit::Unlimited,
+        )
+        .expect("could not create host");
 
     loop {
-        let now = Instant::now();
-        let delay = now - last_updated;
-        server.update(delay);
-        transport.update(delay, &mut server);
-        last_updated = now;
-        
-        while let Some(event) = server.get_event() {
-            match event {           
-                ServerEvent::ClientConnected { client_id } => {
-                    println!("Client {client_id} connected");
-                    server.send_message(client_id, 0, "Welcome!");
-                    server.broadcast_message(DefaultChannel::ReliableOrdered, "A user joined to server.");
-
-                }
-                ServerEvent::ClientDisconnected { client_id, reason } => {
-                    println!("Client {client_id} disconnected: {reason}");
-                }
+        // Wait 1 second for any events.
+        match host.service(Duration::from_secs(1)) {
+            Ok(Some(event)) => match &event.kind {
+                &EventKind::Connect => {
+                    println!("new connection!");
+                    let id = event.peer_id;
+                    print!("{:#?}",id);
+                },
+                &EventKind::Disconnect { .. } => println!("disconnect!"),
+                &EventKind::Receive {
+                    ref channel_id,
+                    ref packet,
+                } => {
+                    println!(
+                    "got packet on channel {}, content: '{}'",
+                    channel_id,
+                    std::str::from_utf8(packet.data()).unwrap());
+                    let a = event.peer_id;
+                    print!("{:#?}", a)
+                },
+            },
+            Ok(None) => {
+                // No event occurred within the timeout duration
+            }
+            Err(e) => {
+                // Handle the error from `service`
+                eprintln!("Error servicing host: {:?}", e);
             }
         }
-
-        for client_id in server.clients_id() {
-            while let Some(message) = server.receive_message(client_id, DefaultChannel::ReliableOrdered) {
-                println!("Message: {} {:?}",client_id,message);
-            }
-        }
-
-        transport.send_packets(&mut server);
-        std::thread::sleep(Duration::from_millis(16));
-
     }
 }
---
+++ server/src/types.rs
@@ -0,0 +1,1 @@
+type PlayerID = u64;
