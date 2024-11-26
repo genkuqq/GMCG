7df3c7b72c524895ed8a5c1fcae5f89488bd70c1
genkuqq <koraygench@gmail.com>
2024-11-26 21:23:58 +0300

    add: new types

--- server/src/types.rs
+++ server/src/types.rs
@@ -1,1 +1,23 @@
+use std::collections::HashMap;
+
 type PlayerID = u64;
+enum Suit{
+    Hearts,
+    Diamonds,
+    Clubs,
+    Spades,
+}
+
+struct Card{
+    rank: u8,
+    suit: Suit,
+}
+
+struct Player{
+    id: PlayerID,
+    hand: Vec<Card>,
+}
+
+struct Game{
+    players: HashMap<PlayerID, Player>,
+}
